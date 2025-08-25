e667d29b9bfb9ff76c6762d97eb326c0e03e4681
eedajy <jaderibeiro46@gmail.com>
2025-08-24 22:47:43 -0300

    Update Biblioteca.java

--- src/main/java/br/edu/ifba/saj/fwads/Biblioteca.java
+++ src/main/java/br/edu/ifba/saj/fwads/Biblioteca.java
@@ -1,13 +1,27 @@
 package br.edu.ifba.saj.fwads;
 
-import br.edu.ifba.saj.fwads.model.Autor;
+import br.edu.ifba.saj.fwads.model.Professor;
+import br.edu.ifba.saj.fwads.model.Aluno;
+import br.edu.ifba.saj.fwads.model.Aula;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class Biblioteca {
-    public static ObservableList<Autor> listaAutores = FXCollections.observableArrayList();
+    
+    public static ObservableList<Professor> listaProfessores = FXCollections.observableArrayList();
     static{
         //listaAutores.add(new Autor("Jose","jose@seila.com","123.456.789.-11"));
     }
+
+    public static ObservableList<Aluno> listaAlunos = FXCollections.observableArrayList();
+    static{
+        //listaAutores.add(new Autor("Jose","jose@seila.com","123.456.789.-11"));
+    }
+
+    public static ObservableList<Aula> listaAulas = FXCollections.observableArrayList();
+    static{
+        //listaAutores.add(new Autor("Jose","jose@seila.com","123.456.789.-11"));
+    }
+    
     
 }
