99ab6e8b07f69766dd273f1988aec8efd52fe4ed
eedajy <jaderibeiro46@gmail.com>
2025-09-15 14:41:05 -0300

    Update LoginController.java

--- src/main/java/br/edu/ifba/saj/fwads/controller/LoginController.java
+++ src/main/java/br/edu/ifba/saj/fwads/controller/LoginController.java
@@ -1,39 +1,40 @@
-/**
- * Sample Skeleton for 'Login.fxml' Controller Class
- */
-
 package br.edu.ifba.saj.fwads.controller;
 
 import br.edu.ifba.saj.fwads.App;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
+import javafx.event.ActionEvent;
 
 public class LoginController {
 
-    @FXML // fx:id="txSenha"
-    private PasswordField txSenha; // Value injected by FXMLLoader
-
-    @FXML // fx:id="txUsuario"
-    private TextField txUsuario; // Value injected by FXMLLoader
+    @FXML private TextField txUsuario;
+    @FXML private PasswordField txSenha;
 
     @FXML
-    void entrar(ActionEvent event) {
-        if(txUsuario.getText().equals("admin") && txSenha.getText().equals("admin")){
-            new Alert(AlertType.INFORMATION, "Usuário e senha corretos").showAndWait();
-            App.setRoot("controller/Master.fxml");
-        }else{
-            new Alert(AlertType.ERROR, "Usuário ou senha inválidos").show();
+    public void entrar(ActionEvent event) {
+        String usuario = txUsuario.getText();
+        String senha = txSenha.getText();
+
+        if (usuario.equals("admin") && senha.equals("admin")) {
+            try {
+                App.setRoot("Master");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erro");
+            alert.setHeaderText("Usuário ou senha inválidos");
+            alert.setContentText("Tente novamente.");
+            alert.showAndWait();
         }
     }
 
     @FXML
-    void limparCampos(ActionEvent event) {
+    public void limparCampos() {
         txUsuario.setText("");
         txSenha.setText("");
     }
-
 }
