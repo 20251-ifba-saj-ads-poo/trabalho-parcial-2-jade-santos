d9508790f52913d5bc4710f142a60ae7385bb2f0
eedajy <jaderibeiro46@gmail.com>
2025-09-15 14:42:46 -0300

    Update Aula.java

--- src/main/java/br/edu/ifba/saj/fwads/model/Aula.java
+++ src/main/java/br/edu/ifba/saj/fwads/model/Aula.java
@@ -1,47 +1,57 @@
 package br.edu.ifba.saj.fwads.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class Aula {
     private String nomeAula;
-    private String professor;
     private String horario;
+    private Professor professor;
+    private ObservableList<Aluno> alunosInscritos;
 
-    public Aula(String nomeAula, String horario, String professor) {
+    public Aula(String nomeAula, String horario, Professor professor) {
         this.nomeAula = nomeAula;
         this.horario = horario;
         this.professor = professor;
+        this.alunosInscritos = FXCollections.observableArrayList();
     }
 
-    public String getnomeAula() {
+    public String getNomeAula() {
         return nomeAula;
     }
 
-    public void setnomeAula(String nomeAula) {
+    public void setNomeAula(String nomeAula) {
         this.nomeAula = nomeAula;
     }
 
-    public String gethorario() {
+    public String getHorario() {
         return horario;
     }
 
-    public void sethorario(String horario) {
+    public void setHorario(String horario) {
         this.horario = horario;
     }
 
-    public String getprofessor() {
+    public Professor getProfessor() {
         return professor;
     }
 
-    public void setprofessor(String professor) {
+    public void setProfessor(Professor professor) {
         this.professor = professor;
+    }
+    
+    public ObservableList<Aluno> getAlunosInscritos() {
+        return alunosInscritos;
+    }
+    
+    public void adicionarAluno(Aluno aluno) {
+        if (!alunosInscritos.contains(aluno)) {
+            this.alunosInscritos.add(aluno);
+        }
     }
 
     @Override
     public String toString() {
-        return "Aula [nome da aula=" + nomeAula + ", horário=" + horario + ", Professor:" + professor + "]";
-    }
-
-    public String getNome() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getNome'");
+        return nomeAula + " (" + horario + ") - Professor: " + professor.getNome();
     }
 }
